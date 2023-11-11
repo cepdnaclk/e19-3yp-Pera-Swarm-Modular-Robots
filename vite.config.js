83de3dbc7796dafdac19341b65e2e656462fe30b
Sanka Peeris <sankapeeris360@gmail.com>
2023-11-11 21:40:11 +0530

    feat: create absolute import aliases

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      hooks: "/src/hooks",
+      assets: "/src/assets",
+    },
+  },
 })
