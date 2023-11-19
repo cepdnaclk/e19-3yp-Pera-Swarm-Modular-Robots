f08fa014dcef04ac87fd649d01cf127b57e59276
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-11-20 00:14:22 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,25 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import "./App.css";
 
-import Navbar from "components/Navbar";
+//import Navbar from "components/Navbar";
+
 import Home from "pages/Home";
 import About from "pages/About";
 import Contact from "pages/Contact";
+import Overview from './components/Overview/Overview';
 
 function App() {
 
-  const pages = [Home, About, Contact];
-  const homeRef = useRef(null);
-  const aboutRef = useRef(null);
-  const contactRef = useRef(null);
-  const refs = [homeRef, aboutRef, contactRef];
+  // const pages = [Home, About, Contact];
+  // const homeRef = useRef(null);
+  // const aboutRef = useRef(null);
+  // const contactRef = useRef(null);
+  // const refs = [homeRef, aboutRef, contactRef];
 
   return (
   <>
-    <Navbar refs={refs}/>
-    <div className="fullpage-container">
-    {  
-      pages.map((Page, index)=>(
-      <div
-        className="fullpage" 
-        key={index}
-        ref={refs[index]}
-      >
-        <Page />
-      </div>
-      ))
-    }
-    </div>
+    <Overview/>
+   
   </>
   )
 }
