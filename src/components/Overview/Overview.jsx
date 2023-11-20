6bbdc6228f5a58097b9ca213d6ea940e2701a360
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-11-20 17:54:47 +0530

    header vido updated

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import './Overview.css';
 import video_robot from 'assets/moduler.mp4';
+import video_robot_assemble from 'assets/360vid.mp4';
 
 const Overview = () => {
   return (
     <div className='overview-card'>
       <Navbar/>
         <video className="background-video" autoPlay loop muted>
-          <source src={video_robot} type="video/mp4" />
+          <source src={video_robot_assemble} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         
