9d6970e72f55ed86024630a609ffbabc8c9624d3
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-12-06 13:32:15 +0530

    add basic backend api

---
+++ backend-webapp/src/routes/experiments.js
@@ -0,0 +1,71 @@
+const Experiment = require('../schemas/experiment');
+
+// Create a new experiment
+exports.createExperiment = async (req, res) => {
+    try {
+        const experiment = new Experiment(req.body);
+        await experiment.save();
+        res.status(201).json(experiment);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Get all experiments
+exports.getAllExperiments = async (req, res) => {
+    try {
+        const experiments = await Experiment.find();
+        res.json(experiments);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Get a single experiment by ID
+exports.getExperimentById = async (req, res) => {
+    try {
+        const experiment = await Experiment.findById(req.params.id);
+        if (!experiment) {
+            return res.status(404).json({ message: 'Experiment not found' });
+        }
+        res.json(experiment);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Update an experiment by ID
+exports.updateExperiment = async (req, res) => {
+    try {
+        const experiment = await Experiment.findById(req.params.id);
+        if (!experiment) {
+            return res.status(404).json({ message: 'Experiment not found' });
+        }
+
+        // Update fields based on your requirements
+        experiment.name = req.body.name;
+        experiment.id = req.body.id;
+        experiment.log = req.body.log;
+        experiment.videoFile = req.body.videoFile;
+
+        await experiment.save();
+        res.json(experiment);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Delete an experiment by ID
+exports.deleteExperiment = async (req, res) => {
+    try {
+        const experiment = await Experiment.findById(req.params.id);
+        if (!experiment) {
+            return res.status(404).json({ message: 'Experiment not found' });
+        }
+
+        await experiment.remove();
+        res.json({ message: 'Experiment deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
