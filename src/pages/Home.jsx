8420d27d61cb7a1cefeef0f03d5ca25be25429be
Nandun Nethmina <115539687+nandun00@users.noreply.github.com>
2023-12-11 14:40:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
         />
       }
       <div className="content">
-        <div><video className="home-vid" src="../assets/homevid.mp4"</div>
+        <div className="vid"><video className="home-vid" src="../assets/homevid.mp4"</div>
         <Block
           imageUrl={robot}
           title="Introduction to Swarm modular Robot"
