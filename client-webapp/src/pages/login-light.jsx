087507a754ca9b97dd12cd1d6d6f38ff4d922709
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-12-26 18:50:07 +0530

    modify light login

--- client-webapp/src/pages/login-light.jsx
+++ client-webapp/src/pages/login-light.jsx
@@ -10,7 +10,6 @@ const LoginForm = () => {
       password: "",
     },
 
-    //yup to validate in client side
     validationSchema: Yup.object({
       email: Yup.string()
         .email("Please enter a valid Email address")
@@ -21,61 +20,61 @@ const LoginForm = () => {
     }),
 
     onSubmit: (values) => {
-      console.log(values); //debug
+      console.log(values);
     },
   });
 
   return (
     <div className="login-card">
-        <div className="login-header">
-          <img className="login-logo" src="../src/assets/logo.svg" alt="Logo" />
-          <h1>Pera Swarm Experimentation Hub</h1>
-              <hr className="divider" />
+      <div className="login-header">
+        <img className="login-logo" src="../src/assets/logo.svg" alt="Logo" />
+        <h1>Pera Swarm Experimentation Hub</h1>
+      </div>
+      <hr className="divider" />
 
+      <form autoComplete="on" onSubmit={formik.handleSubmit}>
+        <div className="login-form-items">
+          <label htmlFor="email" className="login-email">
+            Email
+            {formik.touched.email && formik.errors.email ? (
+              <span className="login-error"> *Required</span>
+            ) : null}
+          </label>
+          <br />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="username@email.com"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
         </div>
 
-        <form autoComplete="on" onSubmit={formik.handleSubmit}>
-          <div className="login-form-items">
-            <label htmlFor="email" className="login-email">
-              Email
-            </label>
-            <br></br>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="username@email.com"
-              onChange={formik.handleChange} //used for realtime updates
-              onBlur={formik.handleBlur} //notify the end of editing/typing in a field.(clicked outside)
-              value={formik.values.email}
-            />
-            {formik.touched.email && formik.errors.email ? ( //display error messages
-              <div className="login-error">{formik.errors.email}</div>
-            ) : null}
-          </div>
-
-          <div className="login-form-items">
-            <label htmlFor="password" className="login-password">
-              Password
-            </label>
-            <br></br>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.password}
-            />
+        <div className="login-form-items">
+          <label htmlFor="password" className="login-password">
+            Password
             {formik.touched.password && formik.errors.password ? (
-              <div className="login-error">{formik.errors.password}</div>
+              <span className="login-error"> *Required</span>
             ) : null}
-          </div>
+          </label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="At least 5 characters"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+        </div>
 
-          <input className="login-button" type="submit" value="Login" />
-        </form>
-      </div>
+        <input className="login-button" type="submit" value="Login" />
+      </form>
+    </div>
   );
 };
+
 export default LoginForm;
