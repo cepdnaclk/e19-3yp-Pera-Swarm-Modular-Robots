1288debc8f0770b8acf7aa582d5957c73e022737
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-12-28 11:38:39 +0530

    color modification in admindashboard and experiment card

--- client-webapp/src/pages/adminDashboard.jsx
+++ client-webapp/src/pages/adminDashboard.jsx
@@ -15,8 +15,8 @@ const AdminDashboard = () => {
 
   return (
     <>
-      <div className="bg-gray-50 flex flex-col font-inter gap-[18px] items-center justify-start mx-auto p-3 w-full">
-        <div className="bg-blue-200 flex flex-row items-center justify-start max-w-[1410px] mx-auto p-[3px] md:px-5 rounded-[12px] w-full">
+      <div className="bg-bg flex flex-col font-inter gap-[18px] items-center justify-start mx-auto p-3 w-full">
+        <div className="bg-container flex flex-row items-center justify-start max-w-[1410px] mx-auto p-[3px] md:px-5 rounded-[12px] w-full">
           <div className="flex flex-row md:gap-10 items-center justify-between my-0.5 w-full">
             <Img
               className="h-[75px] md:h-auto object-cover w-[60px]"
@@ -30,25 +30,25 @@ const AdminDashboard = () => {
             />
           </div>
         </div>
-        <div className="bg-gray-50 border border-gray-900_01 border-solid flex flex-col items-center justify-start max-w-[1410px] mx-auto p-3.5 md:px-5 rounded-[12px] w-full">
+        <div className="bg-bg border border-container-accent border-solid flex flex-col items-center justify-start max-w-[1410px] mx-auto p-3.5 md:px-5 rounded-[12px] w-full">
           <div className="flex flex-col gap-[27px] justify-start mb-[3px] mt-2.5 w-full">
             <Text
-              className="mr-[938px] md:text-xl sm:text-[28px] text-[32px] text-gray-900_01 font-serif"
+              className="mr-[938px] md:text-xl sm:text-[28px] text-[32px] text-f font-serif"
               size="txtInterRegular12"
             >
               Incoming Request: John Doe
             </Text>
 
-            <button className="cursor-pointer rounded-md leading-[normal] w-[200px] h-[40px] ml-auto md:text-[19px]  text-center  bg-blue-400 font-serif">
+            <button className="cursor-pointer rounded-md leading-[normal] w-[200px] h-[40px] ml-auto md:text-[19px]  text-center  bg-primary text-bg font-serif  transition ease-in-out delay-100 hover:-translate-y-1 ">
               Setup Experiment
             </button>
           </div>
         </div>
-        <div className="border border-gray-900_01 border-solid flex flex-col items-center justify-start max-w-[1410px] mb-3.5 mx-auto p-3 md:px-5 rounded-[12px] w-full">
+        <div className="border border-container-accent border-solid flex flex-col items-center justify-start max-w-[1410px] mb-3.5 mx-auto p-3 md:px-5 rounded-[12px] w-full">
           <div className="flex flex-col gap-[19px] justify-start mb-[101px] w-full">
             <div className="flex flex-col items-center justify-center p-2.5 w-auto">
               <Text
-                className="md:text-4xl sm:text-[28px] text-[35px] text-gray-900_01 w-auto font-serif"
+                className="md:text-4xl sm:text-[28px] text-[35px] text-f w-auto font-serif"
                 size="txtInterRegular32"
               >
                 Archive
