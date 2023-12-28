5af4312523e20ab8b80fd7e4e64306ad8721aa5c
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-12-28 11:17:09 +0530

    add light and dark theme color pallets to tailwind.config.js

--- client-webapp/tailwind.config.js
+++ client-webapp/tailwind.config.js
@@ -9,6 +9,29 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      'primary': '#006590',
+      'primary-accent': '#FFFFFF',
+      'container': '#C8E6FF',
+      'container-accent': '#001E2E',
+      'error': '#BA1A1A',
+      'error-accent': '#410002',
+      'bg': '#FAFCFF',
+      'bg-accent': '#001F2A',
+      'f': 'black',
+      'f-accent': 'white',
+
+      'dark-primary': '#88CEFF',
+      'dark-primary-accent': '#00344D',
+      'dark-container': '#004C6D',
+      'dark-container-accent': '#C8E6FF',
+      'dark-error': '#FFB4AB',
+      'dark-error-accent': '#690005',
+      'dark-bg': '#001F2A',
+      'dark-bg-accent': '#BFE9FF',
+      'dark-f': 'white',
+      'dark-f-accent': 'black',
+    }
   },
   plugins: [],
 }
