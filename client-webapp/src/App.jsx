e175728159becba417feaf5ae7061b57ab0f7ea3
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-12-29 22:20:28 +0530

    add new pages to app

--- client-webapp/src/App.jsx
+++ client-webapp/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import LoginForm from "../src/pages/login";
 import Header from "./components/header";
+import Sandbox from "../src/pages/sandbox";
+import RobotConfig from "../src/pages/robotConfig";
+import AdminDashboard from "../src/pages/adminDashboard";
 
 function App() {
   return (
     //<div><LoginForm/></div>
     <div>
-      <LoginForm />
+      <RobotConfig />
     </div>
   );
 }
