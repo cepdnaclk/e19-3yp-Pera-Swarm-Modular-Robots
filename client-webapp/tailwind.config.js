a37f8f1814f50aac86497d8bb8126bdde9a59688
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2024-01-06 17:35:58 +0530

    add tailwind to the login instead plain css

--- client-webapp/tailwind.config.js
+++ client-webapp/tailwind.config.js
@@ -5,35 +5,42 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/**/**/*.{js,ts,jsx,tsx,html,mdx}",
-    
   ],
+  darkMode: 'class', // Enable dark mode variant
+  
   theme: {
     extend: {},
-    colors:{
+    colors: {
+      // Light theme color palette
       'primary': '#006590',
       'primary-accent': '#FFFFFF',
       'container': '#C8E6FF',
       'container-accent': '#001E2E',
       'error': '#BA1A1A',
       'error-accent': '#410002',
-      'bg': '#FAFCFF',
+      'bgd': '#FAFCFF',
       'bg-accent': '#001F2A',
       'f': 'black',
       'f-accent': 'white',
       'transparent': 'transparent',
 
-      'dark-primary': '#88CEFF',
-      'dark-primary-accent': '#00344D',
-      'dark-container': '#004C6D',
-      'dark-container-accent': '#C8E6FF',
-      'dark-error': '#FFB4AB',
-      'dark-error-accent': '#690005',
-      'dark-bg': '#001F2A',
-      'dark-bg-accent': '#BFE9FF',
-      'dark-f': 'white',
-      'dark-f-accent': 'black',
-    }
+
+      // Dark theme color palette
+      dark: {
+        'primary': '#88CEFF',
+        'primary-accent': '#00344D',
+        'container': '#004C6D',
+        'container-accent': '#C8E6FF',
+        'error': '#FFB4AB',
+        'error-accent': '#690005',
+        'bgd': '#001F2A',
+        'bg-accent': '#BFE9FF',
+        'f': 'white',
+        'f-accent': 'black',
+        'transparent': 'transparent',
+
+      },
+    },
   },
   plugins: [],
 }
-
