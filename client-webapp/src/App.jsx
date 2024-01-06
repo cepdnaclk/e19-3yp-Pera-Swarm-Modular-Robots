0469d73362fa0e2e33f327210d2768b3a72f0290
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2024-01-06 11:39:24 +0530

    add routers and links to navigate pages

--- client-webapp/src/App.jsx
+++ client-webapp/src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "../src/pages/login";
-import Header from "./components/header";
+
 import Sandbox from "../src/pages/sandbox";
 import RobotConfig from "../src/pages/robotConfig";
 import AdminDashboard from "../src/pages/adminDashboard";
 
 function App() {
   return (
-    //<div><LoginForm/></div>
     <div>
-      <RobotConfig />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/sandbox" element={<Sandbox />} />
+          <Route path="/robotConfig" element={<RobotConfig />} />
+          <Route path="/adminDashboard" element={<AdminDashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+///////////////////////
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+// import DragDrop from "../src/components/dragdrop";
+
+// function App() {
+//   return (
+//     <DndProvider backend={HTML5Backend}>
+//       <div className="App">
+//         <DragDrop />
+//       </div>
+//     </DndProvider>
+//   );
+// }
+
+// export default App;
+//////////////////////
