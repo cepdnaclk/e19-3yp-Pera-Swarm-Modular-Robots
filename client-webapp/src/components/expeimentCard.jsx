7d97be829c255378424551a8e5eb2a6b988c6e63
Geethal Wickramasingha <115541926+GeethalWickramasingha@users.noreply.github.com>
2024-01-21 22:48:29 +0530

    create new case values dor card

--- client-webapp/src/components/expeimentCard.jsx
+++ client-webapp/src/components/expeimentCard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { XMarkIcon } from '@heroicons/react/20/solid';
+import { XMarkIcon } from "@heroicons/react/20/solid";
 
-const ExperimentCard = ({experiment,userRole,handleDelete,handleAccept,handleDecline,handleStartExperiment,handleLog,handleVideo}) => {
-
-  const {name, id, status, attachments, schedule} = experiment;
+const ExperimentCard = ({
+  experiment,
+  userRole,
+  handleDelete,
+  handleAccept,
+  handleReady,
+  handleDecline,
+  handleStartExperiment,
+  handleLog,
+  handleVideo,
+}) => {
+  const { name, id, status, attachments, schedule } = experiment;
 
   const getStatusBadge = (status) => {
     let badgeClass = "";
@@ -35,25 +44,47 @@ const ExperimentCard = ({experiment,userRole,handleDelete,handleAccept,handleDec
     }
 
     return (
-      <span className={`inline-flex items-center text-xs font-medium me-2 px-2.5 py-0.5 rounded-full ${badgeClass}`}>
+      <span
+        className={`inline-flex items-center text-xs font-medium me-2 px-2.5 py-0.5 rounded-full ${badgeClass}`}
+      >
         <span class="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
         {badgeText}
       </span>
     );
   };
 
-
   const getSideButtons = (status, userRole) => {
-    if (userRole === 'admin') {
+    if (userRole === "admin") {
       switch (status) {
         case "pending":
           return (
             <>
-              <button type="button" onClick={() => handleAccept(id)} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+              <button
+                type="button"
+                onClick={() => handleAccept(id)}
+                className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
                 Accept
               </button>
-              <button type="button" onClick={() => handleDecline(id)} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+              <button
+                type="button"
+                onClick={() => handleDecline(id)}
+                className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              >
                 Decline
+              </button>
+            </>
+          );
+
+        case "accepted":
+          return (
+            <>
+              <button
+                type="button"
+                onClick={() => handleReady(id)}
+                className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+              >
+                Mark as Ready
               </button>
             </>
           );
@@ -62,44 +93,66 @@ const ExperimentCard = ({experiment,userRole,handleDelete,handleAccept,handleDec
       }
     }
 
-    if (userRole === 'experimenter') {
+    if (userRole === "experimenter") {
       switch (status) {
         case "pending":
-          return (
-            <>
-
-            </>
-          );
+          return <></>;
 
         case "ready":
           return (
             <>
-              <button type="button" onClick={() => handleStartExperiment(id)} className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Experiment</button>
+              <button
+                type="button"
+                onClick={() => handleStartExperiment(id)}
+                className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Start Experiment
+              </button>
+              <button
+                type="button"
+                onClick={() => handleDelete(id)}
+                className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              >
+                Delete Experiment
+              </button>
             </>
           );
 
         case "accepted":
-          return (
-            <>
-              
-            </>
-          );
+          return <></>;
 
         case "declined":
           return (
             <>
-              
+              <button
+                type="button"
+                onClick={() => handleDelete(id)}
+                className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              >
+                Delete Experiment
+              </button>
             </>
           );
 
         case "completed":
           return (
             <>
-              <button type="button" onClick={() => handleLog(id)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Log</button>
-              <button type="button" onClick={() => handleVideo(id)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Video File</button>
+              <button
+                type="button"
+                onClick={() => handleLog(id)}
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                Log
+              </button>
+              <button
+                type="button"
+                onClick={() => handleVideo(id)}
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                Video File
+              </button>
             </>
           );
-
 
         default:
           break;
@@ -107,36 +160,32 @@ const ExperimentCard = ({experiment,userRole,handleDelete,handleAccept,handleDec
     }
   };
 
-
-
   return (
     <a className="relative font-normal font-sans text-mainText block my-3 p-6 bg-secondary border border-gray-200 rounded-lg shadow hover:bg-gray-100  border-ternary hover:bg-mainText/20">
-          
-          {(status !== 'ready' && userRole !== 'admin') && (
-            <div className="absolute z-10 top-0 right-0 m-2">
-              <button type="button" onClick={()=> handleDelete(id)} className="focus:outline-none text-red-900 font-medium rounded-full text-sm w-5 h-auto bg-red-500/5 hover:bg-red-600 ">
-                <XMarkIcon className="w-5 text-mainText/50 " />
-              </button>
-            </div>
-          )}
+      {status !== "ready" && userRole !== "admin" && (
+        <div className="absolute z-10 top-0 right-0 m-2">
+          <button
+            type="button"
+            onClick={() => handleDelete(id)}
+            className="focus:outline-none text-red-900 font-medium rounded-full text-sm w-5 h-auto bg-red-500/5 hover:bg-red-600 "
+          >
+            <XMarkIcon className="w-5 text-mainText/50 " />
+          </button>
+        </div>
+      )}
 
       <div className="grid grid-cols-2">
         <h5 className="mb-2 text-2xl font-bold tracking-tight ">{name}</h5>
-        <div className=" text-right">
-          {getStatusBadge(status)}
-          
-        </div>
+        <div className=" text-right">{getStatusBadge(status)}</div>
       </div>
       <div className="grid grid-cols-2">
         <div className=" bg-gray-100/10 p-4 rounded-lg shadow-md">
           <p className="text-lg font-semibold mb-2">Experiment ID: {id}</p>
           <p className="text-sm ">Schedule: {schedule}</p>
         </div>
-        <div className=" text-right">
-          {getSideButtons(status, userRole)}                        
-        </div>
+        <div className=" text-right">{getSideButtons(status, userRole)}</div>
       </div>
-      <br/>
+      <br />
       <p className=" ">Attachments : </p>
       <table className="m-2 text-center table-auto border-separate border-spacing-x-3 border ">
         <thead>
@@ -153,8 +202,11 @@ const ExperimentCard = ({experiment,userRole,handleDelete,handleAccept,handleDec
         </thead>
         <tbody>
           <tr>
-            {attachments.map((attachment, index) => ( 
+            {/* {attachments.map((attachment, index) => ( 
               <td key={index}>{attachment}</td>
+            ))} */}
+            {attachments.map((attachment) => (
+              <td>{attachment}</td>
             ))}
           </tr>
         </tbody>
